1eea9f435ba516b31147cc1e36a6f1b7975a5bce
caio souza <caiocbahia@hotmail.com>
2022-05-31 11:30:37 -0300

    Getters & Setters

---
+++ S6 objetos e prototypes/Getters e Setters/ex1.js
@@ -0,0 +1,38 @@
+function Produto(nome, preco, estoque) {
+  this.nome = nome;
+  this.preco = preco;
+
+  let estoquePrivado = estoque;
+  Object.defineProperty(this, "estoque", {
+    enumerable: true, //mostra a chave
+    configurable: true, //configurável
+    get: function () {
+      return estoquePrivado;
+    },
+    set: function (valor) {
+      if (typeof valor !== "number") {
+        throw new TypeError("Mensagem");
+      }
+
+      estoquePrivado = valor;
+    },
+  });
+}
+
+function criaProduto() {
+  return {
+    get nome() {
+      return this.nome;
+    },
+    set nome(valor) {
+      nome = valor;
+    },
+  };
+}
+
+// const p1 = new Produto('Camiseta', 20, 3);
+// console.log(p1)
+// p1.estoque = 'dsadsa';
+// console.log(p1.estoque)
+const p2 = criaProduto("Camiseta");
+console.log(p2);
