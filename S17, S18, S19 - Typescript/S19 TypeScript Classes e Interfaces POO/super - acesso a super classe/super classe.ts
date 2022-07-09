8c0d77a9b2237dd830be3842a9e28088eaa29cdf
caio souza <caiocbahia@hotmail.com>
2022-07-08 23:17:36 -0300

    lesson completed

--- S17, S18, S19 - Typescript/S19 TypeScript Classes e Interfaces POO/super - acesso a super classe/super classe.ts
+++ S17, S18, S19 - Typescript/S19 TypeScript Classes e Interfaces POO/super - acesso a super classe/super classe.ts
@@ -23,9 +23,24 @@ export class Pessoa {
   }
 }
 
+//utilizamos o termo 'super' para ter acesso a superclasse
 export class Aluno extends Pessoa {
+  sala: string;
+
+  constructor(
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    cpf: string,
+    sala: string,
+  ) {
+    super(nome, sobrenome, idade, cpf);
+    this.sala = sala;
+  }
   getNomeCompleto(): string {
-    return 'Dados do aluno: ' + this.nome + ' ' + this.sobrenome;
+    console.log('FAZENDO ALGO ANTES');
+    const result = super.getNomeCompleto();
+    return result + ' Heyyyy';
   }
 }
 export class Cliente extends Pessoa {
@@ -35,9 +50,10 @@ export class Cliente extends Pessoa {
 }
 
 const pessoa = new Pessoa('Alcindo', 'Oliveira', 30, '294.256.255.82');
-const aluno = new Aluno('Alcindo', 'Oliveira', 30, '294.256.255.82');
+const aluno = new Aluno('Alcindo', 'Oliveira', 30, '294.256.255.82', '0001');
 const cliente = new Cliente('Alcindo', 'Oliveira', 30, '294.256.255.82');
 
 console.log(pessoa.getNomeCompleto());
 console.log(aluno.getNomeCompleto());
 console.log(cliente.getNomeCompleto());
+console.log(aluno);
